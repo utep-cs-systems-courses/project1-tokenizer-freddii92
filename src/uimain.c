19b3e0baa91221430272a4eb597074b83c03d8c6
freddii92 <arodriguez242@miners.utep.edu>
2020-09-18 20:25:07 -0600

    removed print statements used for testing

--- src/uimain.c
+++ src/uimain.c
@@ -7,13 +7,6 @@ int main()
   printf("> ");
   scanf("%[^\n]", str);
   printf("%s\n", str);
-
-  printf("%d\n", space_char(str[0]));
-  printf("%d\n", non_space_char(str[0]));
-  printf("%s\n", word_start(str));
-  printf("%c\n", *word_terminator(str));
-  printf("%d\n", count_words(str));
-  printf("%s\n", copy_str(str,5));
   print_tokens(tokenize(str));
   free_tokens(tokenize(str));
 }
