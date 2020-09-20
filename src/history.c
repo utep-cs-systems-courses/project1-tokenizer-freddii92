3138dad77f236eb7d949f4634d3832e54ed74e54
freddii92 <arodriguez242@miners.utep.edu>
2020-09-20 11:15:13 -0600

    function works correctly now. added print statements to test

--- src/history.c
+++ src/history.c
@@ -3,9 +3,8 @@
 #include "history.h"
 
 List* init_history() {
-  struct s_List *head = (struct s_List*)malloc(sizeof(struct s_List));
-  head->root = NULL;
-  return head;
+  struct s_List *list = (struct s_List*)malloc(sizeof(struct s_List));
+  return list;
 }
 
 void add_history(List *list, char *str)
@@ -17,18 +16,19 @@ void add_history(List *list, char *str)
     head->id = 0;
     head->str = str;
     head->next = NULL;
+    printf("%s\n",head->str);
+    printf("%d\n",head->id);
   }
   else {
-    printf("%s\n",list->root->str);
     struct s_Item *last = list->root;
 
     while (last->next != NULL) {
       last = last->next;
     }
     last->next = (struct s_Item*)malloc(sizeof(struct s_Item));
-    last->next->id = last->id + 1;
-    last->next->str = str;
-    last->next->next = NULL;
+    last->id = last->id + 1;
+    last->str = str;
+    last->next = NULL;
     printf("%s\n",last->str);
     printf("%d\n", last->id);
   }
@@ -36,11 +36,9 @@ void add_history(List *list, char *str)
 
 int main()
 {
-  struct s_List *start = (struct s_List*)malloc(sizeof(struct s_List));;
-  //start->root = (struct s_Item*)malloc(sizeof(struct s_Item));
-  //start->root->id = 100;
-  //printf("%d\n",start->root->id);
+  struct s_List *start = init_history();
   add_history(start,"hello dude");
   add_history(start,"whats up?");
   add_history(start,"guess what");
+  add_history(start,"this must have id 3");
 }
