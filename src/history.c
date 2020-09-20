ab751016e8b570fec9a17e434d7805256eb510ea
freddii92 <arodriguez242@miners.utep.edu>
2020-09-20 12:10:37 -0600

    added get_history function. list won't go to root. will modify add_history function

--- src/history.c
+++ src/history.c
@@ -9,7 +9,6 @@ List* init_history() {
 
 void add_history(List *list, char *str)
 {
-  
   if (list->root == NULL) {
     list->root = (struct s_Item*)malloc(sizeof(struct s_Item));
     struct s_Item *head = list->root;
@@ -25,7 +24,6 @@ void add_history(List *list, char *str)
     while (last->next != NULL) {
       last = last->next;
     }
-    last->next = (struct s_Item*)malloc(sizeof(struct s_Item));
     last->id = last->id + 1;
     last->str = str;
     last->next = NULL;
@@ -34,11 +32,26 @@ void add_history(List *list, char *str)
   }
 }
 
+char *get_history(List *list, int id)
+{
+  struct s_Item *temp = list->root;
+  printf("%s\n", temp->str);
+  printf("%d\n", temp->id);
+  while (temp->next != NULL) {
+    if (temp->id == id) {
+      return temp->str;
+    }
+    temp = temp->next;
+  }
+  return "Not found";
+}
+
 int main()
 {
-  struct s_List *start = init_history();
+  struct s_List *start = (struct s_List*)malloc(sizeof(struct s_List));
   add_history(start,"hello dude");
   add_history(start,"whats up?");
   add_history(start,"guess what");
   add_history(start,"this must have id 3");
+  printf("%s\n",get_history(start,3));
 }
