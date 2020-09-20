3bf4beecdaadaa8d53a48798818515124b4906c9
freddii92 <arodriguez242@miners.utep.edu>
2020-09-20 13:55:07 -0600

    completed history.c. removed print statements used for testing

--- src/history.c
+++ src/history.c
@@ -14,8 +14,6 @@ void add_history(List *list, char *str)
     list->root->id = 0;
     list->root->str = str;
     list->root->next = NULL;
-    printf("%s\n", list->root->str);
-    printf("%d\n", list->root->id);
   }
   else {
     struct s_Item *last = list->root;
@@ -27,8 +25,6 @@ void add_history(List *list, char *str)
     last->next->id = last->id + 1;
     last->next->str = str;
     last->next->next = NULL;
-    printf("%s\n", last->next->str);
-    printf("%d\n", last->next->id);
   }
 }
 
@@ -48,19 +44,19 @@ char *get_history(List *list, int id)
 void print_history(List *list)
 {
   struct s_Item *temp = list->root;
-  while ( temp != NULL) {
+  while (temp != NULL) {
     printf("%s\n", temp->str);
     temp = temp->next;
   }
 }
 
-int main()
+void free_history(List *list)
 {
-  struct s_List *start = (struct s_List*)malloc(sizeof(struct s_List));
-  add_history(start,"hello dude");
-  add_history(start,"whats up?");
-  add_history(start,"guess what");
-  add_history(start,"this must have id 3");
-  printf("%s\n",get_history(start,3));
-  print_history(start);
+  struct s_Item *temp = list->root;
+  while (temp != NULL) {
+    free(temp->str);
+    free(temp);
+    temp = temp->next;
+  }
+  free(list);
 }
