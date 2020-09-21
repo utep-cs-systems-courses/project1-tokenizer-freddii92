d7fc7559aa4ac53cd98bbc3f8a99922e36417048
freddii92 <arodriguez242@miners.utep.edu>
2020-09-21 15:29:47 -0600

    added loop to store multiple sentences to history

--- src/uimain.c
+++ src/uimain.c
@@ -9,18 +9,22 @@ int main()
   
   puts("Welcome!");
 
-  puts("Please write the sentence you would like tokenized?");
+  puts("Please write 3 sentences you would like tokenized.");
 
+  int i = 0;
   char str[50];
   char **tokens;
+  char copy[50];
+  while (i < 3) {
+    printf("> ");
+    scanf(" %[^\n]", str);
+    tokens = tokenize(str);
+    print_tokens(tokens);
+    add_history(list,copy_str(str, count_words(str)));
+    free_tokens(tokens);
+    i++;
+  }
   
-  printf("> ");
-  scanf(" %[^\n]", str);
-  tokens = tokenize(str);
-  print_tokens(tokens);
-  add_history(list,str);
-  free_tokens(tokens);
-
   char c;
   printf("Would you like to view the history? (y/n)\n>");
   scanf(" %c", &c);
